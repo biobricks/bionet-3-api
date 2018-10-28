4c9f4c164e9ff813cd09310e3858f923ee1650a1
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-10-28 15:49:55 -0700

    update access control response codes

--- modules/apiAccess.js
+++ modules/apiAccess.js
@@ -6,21 +6,21 @@ module.exports = {
     if (process.env.NODE_ENV === "test") {
       next();
     } else if (!req.token) {
-      res.status(401).json({
+      res.status(200).json({
         message: "You do not have access."
       });
     } else {
       jwt.verify(req.token, process.env.JWT_SECRET, (error, decoded) => {
         if (error) {
           console.log(error);
-          res.status(401).json({
+          res.status(200).json({
             message: "You do not have access."
           });
         } else {
           let userId = decoded.sub;
           User.findOne({ _id: userId }).exec((error, user) => {
             if (error || !user) {
-              res.status(401).json({
+              res.status(200).json({
                 message: "You do not have access."
               });
             } else {
@@ -28,7 +28,7 @@ module.exports = {
                 res.locals.currentUser = user;
                 next();
               } else {
-                res.status(401).json({
+                res.status(200).json({
                   message:
                     "You need to be an Admin to perform this action on the API."
                 });
@@ -73,21 +73,21 @@ module.exports = {
     if (process.env.NODE_ENV === "test") {
       next();
     } else if (!req.token) {
-      res.status(401).json({
+      res.status(200).json({
         message: "You do not have access."
       });
     } else {
       jwt.verify(req.token, process.env.JWT_SECRET, (error, decoded) => {
         if (error) {
           console.log(error);
-          res.status(401).json({
+          res.status(200).json({
             message: "You do not have access."
           });
         } else {
           let userId = decoded.sub;
           User.findOne({ _id: userId }).exec((error, user) => {
             if (error || !user) {
-              res.status(401).json({
+              res.status(200).json({
                 message: "You do not have access."
               });
             } else {
@@ -98,7 +98,7 @@ module.exports = {
                 res.locals.currentUser = user;
                 next();
               } else {
-                res.status(401).json({
+                res.status(200).json({
                   message:
                     "You need to be the owner or an Admin to perform this action on the API."
                 });
