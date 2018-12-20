c31bf11433ae7430ad71c9d62c4a2e591a261410
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-12-20 01:35:18 -0800

    update get children

--- modules/fetch.js
+++ modules/fetch.js
@@ -5,10 +5,8 @@ const Physical = require("../models/Physical");
 const Virtual = require("../models/Virtual");
 
 const mongoFetch = {
-  tree: {},
-  fetchAll: async (Model) => { // making a /labs route to test require and use of this function
+  fetchAll: async (Model) => {
     let results;
-    // each model requires different attribute population
     switch (Model) {
       case Lab:
         results = await Model.find().populate({
@@ -56,136 +54,139 @@ const mongoFetch = {
   fetchOne: async (Model, id) => {
     let isTestMode = process.env.NODE_ENV === 'test';
     let result;
-      // each model requires different attribute population
-      if (!isTestMode) {
-        switch (Model) {
-          case Lab:
-            result = await Model.findOne({_id: id}).populate({
-              path: 'users',
-              select: '_id username'
-            }).populate({
-              path: 'joinRequests',
-              select: '_id username'
-            });
-            result['children'] = await getChildren(result);
-            // mongoFetch.tree = result;
-            // await getChildren(result)
-            // result = mongoFetch.tree;
-            break;
-          case Container:
-            result = await Model.findOne({_id: id}).populate({
-              path: 'parent',
-              select: '_id name'
-            }).populate({
-              path: 'creator',
-              select: '_id username'
-            }).populate({
-              path: 'lab',
-              select: '_id name'
-            });
-            result['children'] = await getChildren(result);
-            break;  
-          case Physical:
-            result = await Model.findOne({_id: id}).populate({
-              path: 'parent',
-              select: '_id name'
-            }).populate({
-              path: 'creator',
-              select: '_id username'
-            }).populate({
-              path: 'lab',
-              select: '_id name'
-            }).populate('virtual');  
-            break;  
-          case Virtual:
-            result = await Model.findOne({_id: id}).populate({
-              path: 'creator',
-              select: '_id username'
-            });  
-            break;      
-          default:
-            result = null;
-        }
-      } else {
-        // test mode without populate
-        result = await Model.findOne({_id: id});
-      }  
-      return result;
+    let allContainers = await getAll(Container);
+    let allPhysicals = await getAll(Physical);
+    if (!isTestMode) {
+      switch (Model) {
+        case Lab:
+          result = await Model.findOne({_id: id}).populate({
+            path: 'users',
+            select: '_id username'
+          }).populate({
+            path: 'joinRequests',
+            select: '_id username'
+          });
+          result['children'] = await getChildren(result, allContainers, allPhysicals);
+          break;
+        case Container:
+          result = await Model.findOne({_id: id}).populate({
+            path: 'parent',
+            select: '_id name'
+          }).populate({
+            path: 'creator',
+            select: '_id username'
+          }).populate({
+            path: 'lab',
+            select: '_id name'
+          });
+          result['children'] = await getChildren(result, allContainers, allPhysicals);
+          break;  
+        case Physical:
+          result = await Model.findOne({_id: id}).populate({
+            path: 'parent',
+            select: '_id name'
+          }).populate({
+            path: 'creator',
+            select: '_id username'
+          }).populate({
+            path: 'lab',
+            select: '_id name'
+          }).populate('virtual');  
+          break;  
+        case Virtual:
+          result = await Model.findOne({_id: id}).populate({
+            path: 'creator',
+            select: '_id username'
+          });  
+          break;      
+        default:
+          result = null;
+      }
+    } else {
+      // test mode without populate
+      result = await Model.findOne({_id: id});
+    }  
+    return result;
   }
 };
 
 module.exports = mongoFetch;
 
-async function getChildren(record) {
-
-  // filter all containers into children of record
-  let allContainers = await mongoFetch.fetchAll(Container);
-  let containers = [];
-  for(let i = 0; i < allContainers.length; i++){
-    let container = allContainers[i];
-    let containerChildOfLab = container.parent === null;
-    let containerMatchesParent = containerChildOfLab ? String(container.lab._id) === String(record._id) : String(container.parent._id) === String(record._id);
-    if (containerMatchesParent) {
-      container.children = await getChildren(container);
-      containers.push(container);
-    }
+async function getAll(Model) {
+  let results;
+  switch (Model) {
+    case Lab:
+      results = await Model.find().populate({
+        path: 'users',
+        select: '_id username'
+      }).populate({
+        path: 'joinRequests',
+        select: '_id username'
+      });
+      break;
+    case Container:
+      results = await Model.find().populate({
+        path: 'parent',
+        select: '_id name'
+      }).populate({
+        path: 'creator',
+        select: '_id username'
+      }).populate({
+        path: 'lab',
+        select: '_id name'
+      });
+    case Physical:
+      results = await Model.find().populate({
+        path: 'parent',
+        select: '_id name'
+      }).populate({
+        path: 'creator',
+        select: '_id username'
+      }).populate({
+        path: 'lab',
+        select: '_id name'
+      }).populate('virtual');  
+      break; 
+    case Virtual:
+      results = await Model.find().populate({
+        path: 'creator',
+        select: '_id username'
+      });  
+      break;    
+    default:
+      results = null;
   }
-
-  // filter all physicals into children of record
-  let allPhysicals = await mongoFetch.fetchAll(Physical);
-  let physicals = [];
-  for(let i = 0; i < allPhysicals.length; i++){
-    let physical = allPhysicals[i];
-    if (physical.parent !== null) {
-      if (String(physical.parent._id) === String(record._id)) {
-        physicals.push(physical);
-      }
-    }
-  }
-
-  let result = { containers, physicals };
-  return result;
+  return results;
 }
 
-// let tree = [];
-// async function getChildren(id) {
-//   try {
-//     let record;
-//   } catch (error) {
-//     console.log('getChildren error', error);
-//     return;
-//   }
-// }
+async function getChildren(record, allContainers, allPhysicals) {
+  try {
+    // filter all containers into children of record
+    let containers = [];
+    for(let i = 0; i < allContainers.length; i++){
+      let container = allContainers[i];
+      let containerChildOfLab = container.parent === null;
+      let containerMatchesParent = containerChildOfLab ? String(container.lab._id) === String(record._id) : String(container.parent._id) === String(record._id);
+      if (containerMatchesParent) {
+        container.children = await getChildren(container, allContainers, allPhysicals);
+        containers.push(container);
+      }
+    }
 
-// example from other file
-// provides the structure, need to move it to this context
-// let pathGlobal = []; 
+    // filter all physicals into children of record
+    let physicals = [];
+    for(let i = 0; i < allPhysicals.length; i++){
+      let physical = allPhysicals[i];
+      if (physical.parent !== null) {
+        if (String(physical.parent._id) === String(record._id)) {
+          physicals.push(physical);
+        }
+      }
+    }
 
-// async function populateParentContainers(id) {
-//   try {
-//     let item = await Container.findOne({'_id': id});
-//     pathGlobal.push(item);
-//     if (item.parent !== null){
-//       await populateParentContainers(item.parent); 
-//     } else {
-//       return null;
-//     }
-//   } catch (error) {
-//     return null;
-//   }  
-// }
-
-// let allChildren = [];
-
-// getChildren: async (parent) => { 
-//   await Containers.findAll({parent: parent._id}, (err, children) => {
-//      for(i = 0, i < children.length; i++) {
-//        allChildren.push(children[i]);
-//        getChildren(children[i]);
-//      }
-//   }
-// } just notes comparing with what you are about to do
-// go ahead and drop it in if you understand what comes next
-// Oh I dont, i just am studying and keeping this here for my notes.
-// Well you dropped the correct code snippet in, this is the part where that logic goes in,
-// let's see if I can copy and paste, then modify the naming convention 
+    let result = { containers, physicals };
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
